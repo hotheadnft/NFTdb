0d981ff5bb4ec46a0dd2bf8f80ff9af256948621
steven-slater <sslater98@gmail.com>
2021-11-21 07:19:08 -0500

    Add project files.

---
+++ SQLConnection.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.SQLite;
+
+namespace NFTdb_init
+{
+    internal class SQLConnection
+    {
+        private SQLiteConnection Connection { get; set; }
+
+        public SQLiteConnection openDB(string dbfile)
+        {
+            Connection = new SQLiteConnection(dbfile);
+            return Connection;
+        }
+    }
+}
